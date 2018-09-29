f72de04db58e067edbd727c7f17deb8e7bba5466
mcsoini <soini@posteo.de>
2018-09-29 10:40:40 +0200

    switch to config_local.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys, os
-
-raise RuntimeError('Please set BASE_DIR variable in '
-                   'PROFILE_READER/config.py, e.g. '
-                   'BASE_DIR = os.path.abspath(\'../../DATA/\')')
+try:
+    import PROFILE_READER.config_local as conf_local
+    BASE_DIR = conf_local.BASE_DIR
+except:
+    raise RuntimeError('Please set BASE_DIR variable in '
+                       'PROFILE_READER/config_local.py, e.g. '
+                       'BASE_DIR = os.path.abspath(\'../../DATA/\')')
 
 
 
